ee466c87d8d51249ffa2d847bd65811b119b4202
Adam Kimball <ad00m@Adams-MacBook-Pro.local>
2017-08-29 22:56:12 -0600

    added more CSS

--- app/assets/javascripts/athletes.js
+++ app/assets/javascripts/athletes.js
@@ -1,2 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function showLikes() {
+  document.getElementById('#like-table').style.visibility = "visible";
+}
